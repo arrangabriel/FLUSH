ec4f9ae8f49e6d1aa6b4a33aacec1b0fb14a5235
Arran Ø. K. Gabriel <arrankg@gmail.com>
2022-03-30 14:26:22 +0200

    Create generate prompt function

--- flush.c
+++ flush.c
@@ -1,11 +1,20 @@
 #include "parsing.h"
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+void generate_prompt(char *prmpt, size_t sz)
+{
+    bzero(prmpt, sz);
+    getcwd(prmpt, sz);
+    strcat(prmpt, ": ");
+}
 
 int main(int argc, char *argv[]) {
-    char *prmpt = ">";
+    char prmpt[1024];
     char buff[1024];
     int status;
+    generate_prompt(prmpt, sizeof(prmpt));
     while ((status = get_line(prmpt, buff, sizeof(buff))) != NO_INPUT)
     {
         if (status == TOO_LONG) {

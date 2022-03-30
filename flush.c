a6f4b980955e9b1fb56b9a3cf729e17987062c81
Areskiko <39183620+Areskiko@users.noreply.github.com>
2022-03-30 14:05:21 +0200

    Remove unused arm

--- flush.c
+++ flush.c
@@ -8,9 +8,7 @@ int main(int argc, char *argv[]) {
     char buff[1024];
     int status;
     while ((status = getLine(prmpt, buff, sizeof(buff))) != NO_INPUT) {
-        if (status == NO_INPUT) {
-            printf("No input\n");
-        } else if (status == TOO_LONG) {
+        if (status == TOO_LONG) {
             printf("Too long\n");
         } else {
             printf("%s\n", buff);
